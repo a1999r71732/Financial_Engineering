33af522df07bbec38df655a7d34436bc26748619
a1999r71732 <61774523+a1999r71732@users.noreply.github.com>
2020-03-17 22:50:50 +0800

    HW1
    修正浮點數

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,7 +1,7 @@
 import math
 
 print("本金（萬元）：")     #輸入本金
-money = int(input()) * 10000
+principle = float(input()) * 10000
 
 print("期數（年）：")     #輸入期數
 years = int(input()) * 12
@@ -11,18 +11,18 @@ rate = float(input()) * 0.01
 
 list=[]
 c = 0
-c = money/years
+c = principle/years
 
 s = 0
 
 for i in range(years-1):         #除了最後一期之外的本金平均攤還、利息、本利和
-    list.append([i+1,math.ceil(c),round(money*rate/12)])
-    s += math.ceil(c)+round(money*rate/12)
-    money -= math.ceil(c)
+    list.append([i+1,math.ceil(c),round(principle*rate/12)])
+    s += math.ceil(c)+round(principle*rate/12)
+    principle -= math.ceil(c)
     list[i].append(s)
 
-s += money + round(money*rate/12)
-list.append([years,money,round(money*rate/12),s])     #最後一期的本金、利息、本利和
+s += int(principle) + round(principle*rate/12)     #principle是浮點數，最後一期金額一定是整數因此用int
+list.append([years,int(principle),round(principle*rate/12),s])     #最後一期的本金、利息、本利和
 
 for j in range(years):     #輸出結果
     print("第" + str(list[j][0]) + "期", "本金（元）：" + str(list[j][1]), "利息（元）：" + str(list[j][2]), "本金利息累計（元）：" + str(list[j][3]))
