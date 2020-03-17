d737032b90e8f045af1c8886688ee4933c4f2e9a
a1999r71732 <61774523+a1999r71732@users.noreply.github.com>
2020-03-17 23:10:58 +0800

    HW1
    修正

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -16,16 +16,15 @@ c = principle/years
 s = 0
 
 for i in range(years-1):         #除了最後一期之外的本金平均攤還、利息、本利和
-    list.append([i+1,math.ceil(c),round(principle*rate/12)])
     s += math.ceil(c)+round(principle*rate/12)
+    
+    list.append([i+1,math.ceil(c),round(principle*rate/12),s])
     principle -= math.ceil(c)
-    list[i].append(s)
+    
 
 s += int(principle) + round(principle*rate/12)     #principle是浮點數，最後一期金額一定是整數因此用int
 list.append([years,int(principle),round(principle*rate/12),s])     #最後一期的本金、利息、本利和
 
 for j in range(years):     #輸出結果
     print("第" + str(list[j][0]) + "期", "本金（元）：" + str(list[j][1]), "利息（元）：" + str(list[j][2]), "本金利息累計（元）：" + str(list[j][3]))
-
-    
 
